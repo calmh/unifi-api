e7a42efb83e758e11716e913e57efe9c4a5ec044
Jakob Borg <jakob@nym.se>
2012-11-15 10:05:18 +0100

    Setup script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from commands import getoutput
+
+version = getoutput('git describe --always') or '1.0'
+
+setup(name='unifi',
+      version=version,
+      description='API towards Ubiquity Networks UniFi controller',
+      author='Jakob Borg',
+      author_email='jakob@nym.se',
+      url='https://github.com/calmh/unifi-api',
+      packages=['unifi'],
+      scripts=['unifi-low-rssi-reconnect', 'unifi-ls-clients'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: System :: Networking']
+     )
